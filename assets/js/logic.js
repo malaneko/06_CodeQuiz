280a0747b0f0c8f7c9d995b25d445b795e4c659f
Magda KO <79726830+malaneko@users.noreply.github.com>
2023-04-25 23:06:49 +0100

    first commit & TBC

---
+++ assets/js/logic.js
@@ -0,0 +1,22 @@
+// Access elements - by using id
+
+//button
+var startButton = document.getElementById('start');
+
+
+
+//
+var startScreen = document.getElementById('start-screen');
+
+var questions = document.getElementById('questions');
+
+var choices = document.getElementById('choices');
+
+var endScreen = document.getElementById('end-screen');
+
+var submit = document.getElementById('submit');
+
+var feedback = document.getElementById('feedback');
+
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+var quizInquiries = [
+    {
+    question1: 'What is DOM?',
+    option1: 'Document Object Model (DOM) is a programming interface for web documents',
+    option2: 'Document Only Mode (DOM) is a framework for HTML',
+    option3: 'Data and Objects Methods (DOM) are a set of JS methods for data and objects manipulation'
+},
+{   
+    question2: 'What is API?',
+    option1: 'Advanced Protocol Information (API) is a web address and a reference to a web resource that specifies its location on a computer network ',
+    option2: 'Application Programming Interfaces (APIs) are constructs made available in programming languages to allow developers to create complex functionality more easily. ',
+    option3: 'Accessibility and Performance Instruction (API) is an advanced web design guide for UX designers'
+},
+{
+    question2: 'What is a web browser?',
+    option1: 'A web browser is the same as internet',
+    option2: 'A web browser is a program used to access information on the World Wide Web',
+    option3: 'A web browser is a set of images, text and other elements prototyped and designed to present the internet resources'
+}];
