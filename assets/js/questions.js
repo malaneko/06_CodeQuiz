587ee9792c86783369876c8b56ae44a849fa7c11
Magda KO <79726830+malaneko@users.noreply.github.com>
2023-05-18 18:08:04 +0100

    commit
    Readme and questions.js update

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,19 +1,23 @@
 var quizInquiries = [
     {
-    question1: 'What is DOM?',
-    option1: 'Document Object Model (DOM) is a programming interface for web documents',
-    option2: 'Document Only Mode (DOM) is a framework for HTML',
-    option3: 'Data and Objects Methods (DOM) are a set of JS methods for data and objects manipulation'
+    question: 'What is the DOM?',
+    choices: ['1: Document Object Model (DOM) is a programming interface for web documents',
+              '2: Document Only Mode (DOM) is a framework for HTML',
+              '3: Data and Objects Methods (DOM) are a set of JS methods for data and objects manipulation'],
+    answer:   'option 1 is correct: Document Object Model (DOM) is a programming interface for web documents'
 },
 {   
-    question2: 'What is API?',
-    option1: 'Advanced Protocol Information (API) is a web address and a reference to a web resource that specifies its location on a computer network ',
-    option2: 'Application Programming Interfaces (APIs) are constructs made available in programming languages to allow developers to create complex functionality more easily. ',
-    option3: 'Accessibility and Performance Instruction (API) is an advanced web design guide for UX designers'
+    question: 'What is API?',
+    choices: ['1: Advanced Protocol Information (API) is a web address and a reference to a web resource that specifies its location on a computer network ',
+              '2: Application Programming Interfaces (APIs) are constructs made available in programming languages to allow developers to create complex functionality more easily. ',
+              '3: Accessibility and Performance Instruction (API) is an advanced web design guide for UX designers'],
+    answer:   'option 2 is correct: Application Programming Interfaces (APIs) are constructs made available in programming languages to allow developers to create complex functionality more easily.'     
 },
 {
-    question2: 'What is a web browser?',
-    option1: 'A web browser is the same as internet',
-    option2: 'A web browser is a program used to access information on the World Wide Web',
-    option3: 'A web browser is a set of images, text and other elements prototyped and designed to present the internet resources'
-}];
+    question: 'What is a web browser?',
+    choices: ['1: A web browser is the same as internet',
+              '2: A web browser is a program used to access information on the World Wide Web',
+              '3: A web browser is a set of images, text and other elements prototyped and designed to present the internet resources'],
+    answer:   'option 2 is correct: A web browser is a program used to access information on the World Wide Web'
+}
+];
